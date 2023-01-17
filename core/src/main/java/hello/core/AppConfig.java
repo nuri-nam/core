36e0b5fc6375366229406dee6d1b77ea14f04cfd
nuri <snfl5124@gmail.com>
2023-01-17 16:32:46 +0900

    Spring Container 적용, Bean 출력 테스트

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,37 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemeberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
-   public MemberService memberService(){
-       return new MemberServiceImpl(new MemoryMemeberRepository());
+   @Bean    //spring container에 등록
+   public MemberService memberService(){                    //MemberService 역할
+       return new MemberServiceImpl(memberRepository());    //구현 클래스
    }
 
-   public OrderService orderService() {
-       return new OrderServiceImpl(new MemoryMemeberRepository(), new FixDiscountPolicy());
+   @Bean
+   public MemberRepository memberRepository() {    //MemberRepository 역할
+        return new MemoryMemeberRepository();        //구현 클래스
+    }
+
+   @Bean
+   public OrderService orderService() {                                    //OrderService 역할
+       return new OrderServiceImpl(memberRepository(), discountPolicy());   //구현 클래스
+   }
+
+   @Bean
+   public DiscountPolicy discountPolicy() {    //DiscountPolicy 역할
+//       return new FixDiscountPolicy();          //구현 클래스
+       return new RateDiscountPolicy();
    }
 }
